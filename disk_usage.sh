10728c4151ee8b53de8bf0b068845a7db1270197
AquaCipher <aptorangepanda@protonmail.com>
2025-04-13 16:05:44 -0700

    Create disk_usage.sh
    Send an email to specified local user when disk usage exceeds a certain threshold, 80% in this example.

---
+++ disk_usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Monitors disk usage and sends local mail if usage exceeds threshold
+
+THRESHOLD=80                    # Alert threshold (in %)
+USER_TO_ALERT="aquacipher"    # Local system user to receive the alert
+
+df -h | grep '^/dev/' | while read line; do
+    USAGE=$(echo "$line" | awk '{print $5}' | sed 's/%//')
+    MOUNTPOINT=$(echo "$line" | awk '{print $6}')
+    
+    if [ "$USAGE" -ge "$THRESHOLD" ]; then
+        MESSAGE="[$(date)] Disk usage is at ${USAGE}% on ${MOUNTPOINT}."
+        echo "$MESSAGE" | mail -s "Disk Alert: $MOUNTPOINT" "$USER_TO_ALERT"
+    fi
+done
